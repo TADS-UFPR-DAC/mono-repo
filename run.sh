4cac7a256a79c2500ab0a719710f5f7f7d3d5524
Gabriel Costa Ganassin <gabriel.ganassin@contabilizei.com.br>
2022-08-31 13:43:19 -0300

    ajustes

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
-docker network create --driver bridge gerente-network
-docker run  --name postgres-build-test -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
-docker run --network gerente-network --name services-bd -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -p 5432: -d postgres
+#---------------------------------------------- Networks -----------------------------------------------
+docker network create --driver bridge bantads-network # MAIN NETWORK
+docker network create --driver bridge gerente-network # GERENTE NETWORK
+#---------------------------------------------- Databases ----------------------------------------------
+docker run --name postgres-build-test -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres #BUILD DATABASE
+docker run --network gerente-network --name gerente-bd -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -d postgres #GERENTE DATABASE
+#-------------------------------------------------------------------------------------------------------
+#--------------------------------------------- API GATEWAY ---------------------------------------------
+cd api-gateway
+docker build -t api-gateway . #BUILD API GATEWAY IMAGE
+docker run --network bantads-network --name api-gateway -p 3000:3000 -d api-gateway # DEPLOY CONTAINER
+cd ..
+#-------------------------------------------------------------------------------------------------------
+#--------------------------------------------Gerente Service -------------------------------------------
 cd gerente-service
-sudo sh ./mvnw spring-boot:build-image
-docker run --network gerente-network --name gerente-service -p 5002:5002 -e DATABASE_HOST=gerente-bd -d gerente-service:0.0.1-SNAPSHOT
+sudo sh ./mvnw spring-boot:build-image #BUILD Gerente Service IMAGE
+docker run --network gerente-network --name gerente-service -p 5002:5002 -e DATABASE_HOST=gerente-bd -d gerente-service:0.0.1-SNAPSHOT #DEPLOY CONTAINER
+docker network connect bantads-network gerente-service #ATTACH MAIN NETWORK
+cd ..
+#-------------------------------------------------------------------------------------------------------
