4903bd33dd6b98c91daaa322bfc0b6fefe41ed41
Gabriel Ganssin <gabriel.ganassin@contabilizei.com.br>
2022-09-10 19:49:18 -0300

    changes

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ git clone https://github.com/TADS-UFPR-DAC/gerente-service
 git clone https://github.com/TADS-UFPR-DAC/conta-service
 git clone https://github.com/TADS-UFPR-DAC/auth-service
 git clone https://github.com/TADS-UFPR-DAC/api-gateway
+git clone https://github.com/TADS-UFPR-DAC/cliente-service
 #---------------------------------------------- Networks -----------------------------------------------
 docker network create --driver bridge bantads-network # MAIN NETWORK
 docker network create --driver bridge conta-network # CONTA NETWORK
@@ -15,8 +16,7 @@ docker run --network bantads-network -p 15672:15672 -p 5672:5672 --name rabbit -
 docker run --name postgres-build-test -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres #BUILD DATABASE
 docker run --network conta-network --name conta-bd -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -d postgres #CONTA DATABASE
 docker run --network cliente-network --name cliente-bd -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -d postgres #CONTA DATABASE
-docker run --network gerente-network --name gerente-bd-read -e POSTGRES_DB=read -e POSTGRES_PASSWORD=password -d postgres #GERENTE DATABASE
-docker run --network gerente-network --name gerente-bd-create -e POSTGRES_DB=create -e POSTGRES_PASSWORD=password -d postgres #GERENTE DATABASE
+docker run --network gerente-network --name gerente-bd -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -d postgres #GERENTE DATABASE
 docker run --network auth-network --name auth-bd -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -d postgres #AUTH DATABASE
 
 #-------------------------------------------------------------------------------------------------------
