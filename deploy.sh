e1890c16935d9d43f022e9b21382a75c32f0e16c
Gabriel Ganssin <gabriel.ganassin@contabilizei.com.br>
2022-09-10 19:45:26 -0300

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+git clone https://github.com/TADS-UFPR-DAC/gerente-service
+git clone https://github.com/TADS-UFPR-DAC/conta-service
+git clone https://github.com/TADS-UFPR-DAC/auth-service
+git clone https://github.com/TADS-UFPR-DAC/api-gateway
 #---------------------------------------------- Networks -----------------------------------------------
 docker network create --driver bridge bantads-network # MAIN NETWORK
 docker network create --driver bridge gerente-network # GERENTE NETWORK
 docker network create --driver bridge conta-network # CONTA NETWORK
 docker network create --driver bridge auth-network # AUTH NETWORK
+#----------------------------------------------  RABBITMQ ----------------------------------------------
+docker run --network bantads-network -p 15672:15672 -p 5672:5672 --name rabbit -d rabbitmq:3-management
 #---------------------------------------------- Databases ----------------------------------------------
 docker run --name postgres-build-test -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres #BUILD DATABASE
 docker run --network gerente-network --name gerente-bd -e POSTGRES_DB=postgres -e POSTGRES_PASSWORD=password -d postgres #GERENTE DATABASE
